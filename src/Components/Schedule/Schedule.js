6c2ac547eb5c25ed9374d4cf35911c5dfb507f56
Ansh2 <43502767+Ansh2@users.noreply.github.com>
2022-11-17 22:02:09 -0500

    Update Schedule.js

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -20,68 +20,86 @@ export default class Schedule extends React.Component {
                   
                   <th>Time</th>
                   <th>Event</th>
+                  <th>Day (Sat/Sun)</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   
                   <td>Check-in</td>
-                  <td>8:00am-9:00am</td>
+                  <td>9:00am-10:00am</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
                   <td>Opening Ceremony</td>
-                  <td>9:00am-9:45am</td>
-                </tr>
-                <tr>
-                  
-                  <td>Getting hyped up and ready</td>
-                  <td>9:45am-10:00am</td>
+                  <td>10:00am-10:30am</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
                   <td>Hackathon Commences!</td>
-                  <td>10:00am</td>
+                  <td>10:30am</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
-                  
-                  <td>Workshop: Introduction to Node.js</td>
-                  <td>10:00am-11:00am</td>
+                  <td>Introduction to Python</td>
+                  <td>11:00am - 12:00pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
-                  
-                  <td>Workshop: Introduction to Python</td>
-                  <td>12:00pm-1:00pm</td>
-                </tr>
-                <tr>
-                  
-                  <td>Lunch Time</td>
+                  <td>Introduction to Node.js</td>
                   <td>1:00pm-2:00pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
                   <td>Workshop: Machine Learning and Video Analysis on Microsoft Azure</td>
-                  <td>2:00pm-3:00pm</td>
+                  <td>3:00pm-4:00pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
-                  <td>Dinner Time</td>
-                  <td>5:00pm-6:00pm</td>
+                  <td>Intro to AI/ML</td>
+                  <td>4:30pm - 5:30pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
-                  <td>Submit the projects</td>
+                  <td>Mincreaft Server Opens!</td>
                   <td>6:00pm</td>
+                  <td>Sat</td>
+                </tr>
+
+                <tr>
+                  
+                  <td>Mini-Event #1 - Scribble.io + Chess Competition</td>
+                  <td>7:00pm - 8:00pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
-                  <td>Judging Commences</td>
-                  <td>6:00pm-7:00pm</td>
+                  <td>Mini-Event #2 - Movie Night + Chill</td>
+                  <td>9:00pm-11:30pm</td>
+                  <td>Sat</td>
                 </tr>
                 <tr>
                   
+                  <td>Devpost Tutorial</td>
+                  <td>10:30am - 11:00am</td>
+                  <td>Sun</td>
+                </tr>
+                <tr>
+                  
+                  <td>Submission Time</td>
+                  <td>12:00pm</td>
+                  <td>Sun</td>
+                </tr>
+                <tr>
+                    
                   <td>Closing Ceremony & Awards</td>
-                  <td>7:00pm-8:00pm</td>
+                  <td>5:00pm-6:00pm</td>
+                  <td>Sun</td>
                 </tr>
               </tbody>
             </Table>
